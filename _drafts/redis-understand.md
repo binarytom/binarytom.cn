b4472d013b9b68b19630848991b8b341c041ba7d
李恩贺 <lienhe@bytedance.com>
2024-03-13 11:16:25 +0800

    add some understand files.

--- _drafts/redis-understand.md
+++
@@ -1,5 +0,0 @@
-redis-understand.md
----
----
-
-## 
---
+++ _leetcode/516.最长回文子序列.java
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=java
+ *
+ * [516] 最长回文子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+    }
+}
+// @lc code=end
+
